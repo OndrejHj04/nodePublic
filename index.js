8943a850a149145df33beecf32915b733e82293b
OndrejHj04 <ondra.hajku@seznam.cz>
2023-03-26 14:14:18 +0200

    fix

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const Company = require("./models/company");
+const moment = require("moment");
 dotenv.config();
 mongoose.set("strictQuery", false);
 const app = express();
@@ -11,8 +12,12 @@ app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 const connection = process.env.CONNECTION;
+
 app.get("/", (req, res) => {
-  res.send({ msg: "Welcome", date: new Date().toString()});
+  res.send({
+    msg: "Welcome",
+    date: moment().format("DD. MM. YYYY HH:MM")
+  });
 });
 
 app.get("/api/companies", authenticate, async (req, res) => {
@@ -26,7 +31,11 @@ app.get("/api/companies", authenticate, async (req, res) => {
 });
 
 app.post("/api/add-company", authenticate, async (req, res) => {
-  const company = new Company({...req.body, state: "created", date: new Date()});
+  const company = new Company({
+    ...req.body,
+    state: "created",
+    date: moment().format("DD. MM. YYYY HH:MM"),
+  });
 
   try {
     await company.save();
@@ -67,10 +76,10 @@ app.delete("/api/delete-multiple", authenticate, async (req, res) => {
   }
 });
 
-app.delete("/api/delete-all", authenticate, async (req,res) => {
-  const data = await Company.deleteMany({})
-  res.status(200)
-})
+app.delete("/api/delete-all", authenticate, async (req, res) => {
+  const data = await Company.deleteMany({});
+  res.status(200);
+});
 
 app.get("/api/company/:id", authenticate, async (req, res) => {
   const { id } = req.params;
@@ -88,51 +97,55 @@ app.get("/api/company/:id", authenticate, async (req, res) => {
 
 app.put("/api/change-company/:id", async (req, res) => {
   const { id } = req.params;
-  
-  try{
-    const company = await Company.findOneAndUpdate({_id: id}, {...req.body, lastChange: new Date()}, {returnOriginal: false})
-    res.status(200).json({data: company})
-    
-  }catch(e){
-    res.status(400).json({data: e.message})
+
+  try {
+    const company = await Company.findOneAndUpdate(
+      { _id: id },
+      { ...req.body, lastChange: moment().format("DD. MM. YYYY HH:MM")},
+      { returnOriginal: false }
+    );
+    res.status(200).json({ data: company });
+  } catch (e) {
+    res.status(400).json({ data: e.message });
   }
-})
+});
 
 const start = async () => {
   try {
     await mongoose.connect(connection);
-    app.listen(PORT, ()=>{
-      console.log('pripojeno', PORT)
-    })
+    app.listen(PORT, () => {
+      console.log("pripojeno", PORT);
+    });
   } catch (e) {
     console.log(e);
   }
 };
 
-app.get("/posts", authenticate, (req, res)=>{
-  res.json({data: "data"})
-})
-
-app.post("/login", (req, res) => {
-  const username = req.body.username
-  const user = {name: username}
-  const acessToken = jwt.sign(user, process.env.ACESS_TOKEN_SECRET)
-
-  res.json({acessToken: acessToken})
+app.get("/posts", authenticate, (req, res) => {
+  res.json({ data: "data" });
 });
 
-function authenticate(req, res, next){
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(" ")[1]
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const user = { name: username };
+  const acessToken = jwt.sign(user, process.env.ACESS_TOKEN_SECRET);
 
-  if(token === null) return res.sendStatus(401)
+  res.json({ acessToken: acessToken });
+});
 
-  jwt.verify(token, process.env.ACESS_TOKEN_SECRET, (err, user)=>{
-    if(err) return res.sendStatus(403)
-    req.user = user
-    next()
-  })
+app.post("/register", (req, res) => {});
+
+function authenticate(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token === null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
 
 start();
-
